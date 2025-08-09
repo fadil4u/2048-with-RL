ae70ea644401c5d285ff912eb596dde779083d69
fadil4u <fadilrahman4u@gmail.com>
2025-08-09 14:08:17 +0530

    training

--- model.py
+++ model.py
@@ -16,3 +16,38 @@ class LinearQNet(nn.Module):
     def forward(self, x):
         x = torch.relu(self.linear1(x))
         return self.linear2(x)
+    
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()
+
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx in range(len(done)):
+            q_new = reward[idx]
+            if not done[idx]:
+                q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+            target[idx][action[idx]] = q_new
+
+        self.optimizer.zero_grad()
+        loss = self.criterion(pred, target)
+        loss.backward()
+        self.optimizer.step()
